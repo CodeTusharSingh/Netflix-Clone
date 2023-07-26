742bb86b2ffc64168201e2a46e5dbcab6a48cc82
Tushar Singh <99042369+CodeTusharSingh@users.noreply.github.com>
2023-07-26 23:20:28 +0530

    Add files via upload

---
+++ netflixScript.js
@@ -0,0 +1,67 @@
+"use strict";
+let counter1 = 0;
+document.getElementById("dq1").addEventListener('click', function () {
+    if (counter1 % 2 == 0) {
+        document.getElementById("x1").style.transform = 'rotate(45deg)';
+        document.getElementById("da1").style.display = 'block';
+    } else {
+        document.getElementById("x1").style.transform = 'rotate(90deg)';
+        document.getElementById("da1").style.display = 'none';
+    }
+    counter1++;
+});
+let counter2 = 0;
+document.getElementById("dq2").addEventListener('click', function () {
+    if (counter2 % 2 == 0) {
+        document.getElementById("x2").style.transform = 'rotate(45deg)';
+        document.getElementById("da2").style.display = 'block';
+    } else {
+        document.getElementById("x2").style.transform = 'rotate(90deg)';
+        document.getElementById("da2").style.display = 'none';
+    }
+    counter2++;
+});
+let counter3 = 0;
+document.getElementById("dq3").addEventListener('click', function () {
+    if (counter3 % 2 == 0) {
+        document.getElementById("x3").style.transform = 'rotate(45deg)';
+        document.getElementById("da3").style.display = 'block';
+    } else {
+        document.getElementById("x3").style.transform = 'rotate(90deg)';
+        document.getElementById("da3").style.display = 'none';
+    }
+    counter3++;
+});
+let counter4 = 0;
+document.getElementById("dq4").addEventListener('click', function () {
+    if (counter4 % 2 == 0) {
+        document.getElementById("x4").style.transform = 'rotate(45deg)';
+        document.getElementById("da4").style.display = 'block';
+    } else {
+        document.getElementById("x4").style.transform = 'rotate(90deg)';
+        document.getElementById("da4").style.display = 'none';
+    }
+    counter4++;
+});
+let counter5 = 0;
+document.getElementById("dq5").addEventListener('click', function () {
+    if (counter5 % 2 == 0) {
+        document.getElementById("x5").style.transform = 'rotate(45deg)';
+        document.getElementById("da5").style.display = 'block';
+    } else {
+        document.getElementById("x5").style.transform = 'rotate(90deg)';
+        document.getElementById("da5").style.display = 'none';
+    }
+    counter5++;
+});
+let counter6 = 0;
+document.getElementById("dq6").addEventListener('click', function () {
+    if (counter6 % 2 == 0) {
+        document.getElementById("x6").style.transform = 'rotate(45deg)';
+        document.getElementById("da6").style.display = 'block';
+    } else {
+        document.getElementById("x6").style.transform = 'rotate(90deg)';
+        document.getElementById("da6").style.display = 'none';
+    }
+    counter6++;
+});
